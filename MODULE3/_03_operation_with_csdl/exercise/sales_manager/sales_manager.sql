9d710b42e2af3517a206ec9ad967028b9c9755a6
hoang1341995 <hoang1341995@gmail.com>
2022-05-18 07:59:43 +0700

    update
    update

--- MODULE3/_03_operation_with_csdl/exercise/sales_manager/sales_manager.sql
+++ MODULE3/_03_operation_with_csdl/exercise/sales_manager/sales_manager.sql
@@ -2,42 +2,42 @@ create database sales_manager;
 use sales_manager;
 
 CREATE TABLE customer (
-    cID INT,
-    cName VARCHAR(50) NOT NULL,
-    cAge INT NOT NULL,
-    PRIMARY KEY (cID)
+    cid INT,
+    cname VARCHAR(50) NOT NULL,
+    cage INT NOT NULL,
+    PRIMARY KEY (cid)
 );
 
-CREATE TABLE orderproduct (
-    oID INT,
-    cID INT ,
-    oDate DATE NOT NULL,
-    oTotalPrice DOUBLE,
-    PRIMARY KEY (oID)
+CREATE TABLE order_product (
+    oid INT,
+    cid INT ,
+    odate DATE NOT NULL,
+    ototal_price DOUBLE,
+    PRIMARY KEY (oid)
 );
 
 CREATE TABLE product (
-    pID INT,
-    pName VARCHAR(50) NOT NULL,
-    pPrice DOUBLE,
-    PRIMARY KEY (pID)
+    pid INT,
+    pname VARCHAR(50) NOT NULL,
+    pprice DOUBLE,
+    PRIMARY KEY (pid)
 );
 
-CREATE TABLE orderproductdetail (
-    oID INT,
-    pID INT,
+CREATE TABLE order_product_detail (
+    oid INT,
+    pid INT,
     amount INT,
-    PRIMARY KEY (oID , pID)
+    PRIMARY KEY (oid , pid)
 );
 
 
-alter table orderproduct
-add foreign key (cID) references customer(cID);
+alter table order_product
+add foreign key (cid) references customer(cid);
 
 
-alter table orderproductdetail
-add foreign key (oID) references orderproduct(oID),
-add foreign key (pID) references product(pID);
+alter table order_product_detail
+add foreign key (oid) references order_product(oid),
+add foreign key (pid) references product(pid);
 
 
 insert into customer values (1,"Minh Quan",10);
@@ -45,9 +45,9 @@ insert into customer values (2,"Ngoc Oanh",20);
 insert into customer values (3,"Hong Ha",50);
 
 
-insert into orderproduct values (1,1,'2022-03-21', Null);
-insert into orderproduct values (2,2,'2022-03-23', Null);
-insert into orderproduct values (3,1,'2022-03-16', Null);
+insert into order_product values (1,1,'2022-03-21', Null);
+insert into order_product values (2,2,'2022-03-23', Null);
+insert into order_product values (3,1,'2022-03-16', Null);
 
 insert into product values (1,"May Giat",3);
 insert into product values (2,"Tu Lanh",5);
@@ -56,36 +56,36 @@ insert into product values (4,"Quat",1);
 insert into product values (5,"Bep Dien",2);
 
 
-insert into orderproductdetail values (1,1,3);
-insert into orderproductdetail values (1,3,7);
-insert into orderproductdetail values (1,4,2);
-insert into orderproductdetail values (2,1,1);
-insert into orderproductdetail values (3,1,8);
-insert into orderproductdetail values (2,5,4);
-insert into orderproductdetail values (2,3,3);
+insert into order_product_detail values (1,1,3);
+insert into order_product_detail values (1,3,7);
+insert into order_product_detail values (1,4,2);
+insert into order_product_detail values (2,1,1);
+insert into order_product_detail values (3,1,8);
+insert into order_product_detail values (2,5,4);
+insert into order_product_detail values (2,3,3);
 
 #Hiển thị các thông tin  gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
-SELECT orderproduct.oID, orderproduct.oDate, orderproduct.oTotalPrice FROM orderproduct;
+SELECT order_product.oid, order_product.odate, order_product.ototal_price FROM order_product;
 
 #Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
-SELECT c.cID, c.cName, p.pName, p.pPrice
+SELECT c.cid, c.cname, p.pname, p.pprice
 FROM (((orderproductdetail od
-	INNER JOIN orderproduct o ON od.oID = o.oID)
-	INNER JOIN product p ON od.pID = p.pID)
-	INNER JOIN customer c ON o.cID = c.cID);
+	INNER JOIN order_product o ON od.oid = o.oid)
+	INNER JOIN product p ON od.pid = p.pid)
+	INNER JOIN customer c ON o.cid = c.pid);
 
 #Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
-SELECT customer.cID, customer.cName FROM customer
-WHERE customer.cID NOT IN (SELECT orderproduct.cID FROM orderproduct);
+SELECT customer.cid, customer.cName FROM customer
+WHERE customer.cid NOT IN (SELECT order_product.cid FROM order_product);
 
 #Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn
 #(giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn.
 #Giá bán của từng loại được tính = odQTY*pPrice)
 
-SELECT o.oID Order_ID, 
-	o.oDate Order_Date, 
-    SUM(od.amount * p.pPrice) tong_tien
-FROM ((orderproductdetail od
-	INNER JOIN orderproduct o ON od.oID = o.oID)
-	INNER JOIN product p ON od.pID = p.pID)
-GROUP BY o.oID;
+SELECT o.oid order_id, 
+	o.oDate order_date, 
+    SUM(od.amount * p.pprice) tong_tien
+FROM ((order_product_detail od
+	INNER JOIN order_product o ON od.oid = o.oid)
+	INNER JOIN product p ON od.pid = p.pid)
+GROUP BY o.oid;
