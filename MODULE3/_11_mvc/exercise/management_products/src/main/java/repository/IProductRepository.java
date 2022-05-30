d2ac6bf53210ffd04b9468e127626cacf7ab71f2
hoang1341995 <hoang1341995@gmail.com>
2022-05-30 17:07:41 +0700

    update
    update

---
+++ MODULE3/_11_mvc/exercise/management_products/src/main/java/repository/IProductRepository.java
@@ -0,0 +1,20 @@
+package repository;
+
+import model.Product;
+
+import java.util.List;
+
+public interface IProductRepository {
+
+    List<Product> showAll();
+
+    void addNew(Product customer);
+
+    Product findProduct(int id);
+
+    void update( Product product);
+
+    void remove(int id);
+
+    List<Product> searchProduct(String search);
+}
